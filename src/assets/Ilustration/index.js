f2902663f4c002d3a0f53c6c888d8de699c7516a
“naandalist” <listiananda.apriliawan@gmail.com>
2022-12-13 15:56:29 +0700

    feat: add picture assets

---
+++ src/assets/Ilustration/index.js
@@ -0,0 +1,7 @@
+import Logo from './Logo.svg';
+import IlSuccessSignUp from './SuccessSignUp.svg';
+import IlSuccessOrder from './SuccessOrder.svg';
+import IlEmptyOrder from './EmptyOrder.svg';
+import LogoCooking from './cooking.png';
+
+export {Logo, IlSuccessSignUp, IlSuccessOrder, IlEmptyOrder, LogoCooking};
